7639e81d4683fbd82066648296c321d98290c373
DSA <uranusfor@gmail.com>
2025-11-09 09:59:23 +0530

    Update Majority.py with improvements

--- Arrays/Medium/Majority.py
+++ Arrays/Medium/Majority.py
@@ -10,6 +10,27 @@ def duplicate(arr,n):
     for num,cnt in ha.items():
         if cnt>=i:
             return num,cnt
+
+    # Moore's Voting Algorithm
+    cnt=0
+    ele=None
+    for i in range(n):
+        if (cnt==0):
+            cnt=1
+            ele=arr[i]
+        
+        elif ele==arr[i]:
+            cnt+=1
+        else:
+            cnt-=1
+    
+    cnt1=0
+    for j in range(n):
+        if arr[i]==ele:
+            cnt1+=1
+    if cnt1>(n/2):
+        return ele
+    return -1
     
         
             
